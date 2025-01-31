caedee1bae2b84571eb294e6a427ad3f04a1a4e0
Guisandroni <gui.sandroni@gmail.com>
2025-01-31 16:21:55 -0300

    build production

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 
 import express from 'express'
 import dotenv from 'dotenv'
+import path from 'path'
 import { conectDB } from './config/db.js'
 import produtoRoutes from './routes/produto.routes.js'
 
@@ -12,9 +13,19 @@ const PORT = process.env.PORT || 2000
 app.use('/api/produtos',produtoRoutes)
 
 
+//Config build production for deploy
+const __dirname = path.resolve();
+
+if(process.env.NODE_ENV==="production"){
+    app.use(express.static(path.join(__dirname,"/frontend/dist")))
+
+    app.get("*", (req,res)=> {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist" , "index.html"))
+    })
+}
 
     //console.log(process.env.MONGO_URL)
-app.listen(2000,() =>{
+app.listen(PORT,() =>{
     conectDB();
     console.log("Server is running in http://localhost:" + PORT)
 })
